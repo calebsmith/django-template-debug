6e58c4f9b027a8155e23ad060cc4f05c6d412439
Caleb Smith <csmith@caktusgroup.com>
2012-09-07 23:23:50 -0400

    refs #1 - Cleaned up existing tests. Added a test for flattening generators

--- template_debug/tests/utils.py
+++ template_debug/tests/utils.py
@@ -1,3 +1,5 @@
+from copy import copy
+
 from template_debug.tests.base import TemplateDebugTestCase
 from template_debug.utils import _flatten, get_variables
 
@@ -8,22 +10,27 @@ class FlattenTestCase(TemplateDebugTestCase):
     def test_flattens_inner_list(self):
         "Assure arbitrarily nested lists are flattened"
         nested_list = [1, [2, [3, 4, [5], ], 6, 7], 8]
-        self.assertEqual(list(_flatten(nested_list)), [1, 2, 3, 4, 5, 6, 7, 8])
+        self.assertEqual(list(_flatten(nested_list)), range(1, 9))
 
     def test_flattens_tuples(self):
         "Assure nested tuples are also flattened"
         nested_tuples = (1, (2, 3, (4, ), 5), 6)
-        self.assertEqual(list(_flatten(nested_tuples)), [1, 2, 3, 4, 5, 6])
+        self.assertEqual(list(_flatten(nested_tuples)), range(1, 7))
 
     def test_flattens_sets(self):
         "Assure nested sets are flattened"
         nested_sets = set([1, frozenset([2, 3]), 4])
-        self.assertEqual(list(_flatten(nested_sets)), [1, 2, 3, 4])
+        self.assertEqual(list(_flatten(nested_sets)), range(1, 5))
 
     def test_flatten_nested_combinations(self):
         "Assure nested iterables are flattened"
         nested = [1, frozenset([2, 3]), (4, (5,), 6), [7], 8]
-        self.assertEqual(list(_flatten(nested)), [1, 2, 3, 4, 5, 6, 7, 8])
+        self.assertEqual(list(_flatten(nested)), range(1, 9))
+
+    def test_flatten_generator(self):
+        "Assure generators are flattened"
+        gens = [1, xrange(2, 4), (num for num in (4, xrange(5, 7)))]
+        self.assertEqual(list(_flatten(gens)), range(1, 7))
 
     def test_flatten_string_unchanged(self):
         "Assure strings are left intact"
@@ -55,7 +62,7 @@ class GetVariablesTestCase(TemplateDebugTestCase):
         context processors
         """
         context = self.get_context(url='/a/')
-        expected_context = self.globals
-        self.globals.insert(5, 'a')
-        self.globals.remove('params')
+        expected_context = copy(self.globals)
+        expected_context.insert(5, 'a')
+        expected_context.remove('params')
         self.assertEqual(get_variables(context), expected_context)
